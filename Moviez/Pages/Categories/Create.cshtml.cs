a3900d3d6a70922eb1e706197fff947a7623b935
Zoran <zoranmilovanovic656@gmail.com>
2023-02-16 15:40:15 +0100

    Create property in page model

--- Moviez/Pages/Categories/Create.cshtml.cs
+++ Moviez/Pages/Categories/Create.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moviez.Model;
 
 namespace Moviez.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        public Category Category { get; set; }
         public void OnGet()
         {
         }
