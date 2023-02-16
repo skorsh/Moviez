d95673d9dc300495a1c5c7eb5878cb8d86bce70c
Zoran <zoranmilovanovic656@gmail.com>
2023-02-16 15:51:27 +0100

    Create Category and bind property

--- Moviez/Pages/Categories/Create.cshtml.cs
+++ Moviez/Pages/Categories/Create.cshtml.cs
@@ -1,14 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moviez.Data;
 using Moviez.Model;
 
 namespace Moviez.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
         public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _context.Categories.AddAsync(Category);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
