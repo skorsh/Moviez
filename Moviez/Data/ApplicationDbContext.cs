6a50ba93315ae29686c9fb682da14e96f299af86
Zoran <zoranmilovanovic656@gmail.com>
2023-02-16 13:13:38 +0100

    Connect to database, first migrations

---
+++ Moviez/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moviez.Model;
+
+namespace Moviez.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
