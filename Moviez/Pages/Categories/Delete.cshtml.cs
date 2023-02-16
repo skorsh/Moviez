0bba3adca2161ae664925274999ae5a67f7e7f63
Zoran <zoranmilovanovic656@gmail.com>
2023-02-16 19:09:44 +0100

    Edit and delete category

---
+++ Moviez/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moviez.Data;
+using Moviez.Model;
+
+namespace Moviez.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet(int id)
+        {
+            Category = _context.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+        
+            var categoryFromDb = _context.Categories.Find(Category.Id);
+            if (categoryFromDb != null)
+            {
+                _context.Categories.Remove(categoryFromDb);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ Moviez/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moviez.Data;
+using Moviez.Model;
+
+namespace Moviez.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public void OnGet(int id)
+        {
+            Category = _context.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(Category);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
